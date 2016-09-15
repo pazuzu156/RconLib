5a16822f6a79aa7fd52571aa697aa7ff063cd4b9
Kaleb Klein <klein.jae@gmail.com>
2016-09-15 13:06:50 -0400

    Initial commit
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

---
+++ RconLib/RCON.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pazuzu156.RconLib
+{
+	public class RCON
+	{
+		private Socket _rconSocket;
+
+		public RCON()
+		{
+			this._rconSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		}
+
+		public bool Connect(IPEndPoint server, string rconPassword)
+		{
+			try
+			{
+				this._rconSocket.Connect(server);
+			}
+			catch(SocketException ex)
+			{
+				return false;
+			}
+
+
+
+			return true;
+		}
+	}
+}
---
+++ RconLibTests/RCONConnectionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using Pazuzu156.RconLib;
+
+namespace RconLibTests
+{
+	[TestClass]
+	public class RCONConnectionTest
+	{
+		[TestMethod]
+		public void TestConnection()
+		{
+			IPEndPoint server = new IPEndPoint(IPAddress.Parse("192.168.1.50"), 25575);
+			RCON rcon = new RCON();
+			Assert.IsTrue(rcon.Connect(server, "testpassword"), "connection test");
+		}
+	}
+}
